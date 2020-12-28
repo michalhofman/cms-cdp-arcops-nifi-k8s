93a1c51e8b50e5ef334c7611d1b266667585b027
Adrian Misztal <ams@touk.pl>
2020-12-28 12:12:20 +0100

    CMSMIG-969 Tests

--- arcops-nifi-k8s-processors/src/test/java/com/viacom/arcops/nifi/DatasetReaderProcessorTest.java
+++ arcops-nifi-k8s-processors/src/test/java/com/viacom/arcops/nifi/DatasetReaderProcessorTest.java
@@ -10,9 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static com.viacom.arcops.nifi.DatasetReaderProcessor.BODY_COLUMN;
@@ -20,28 +18,29 @@ import static com.viacom.arcops.nifi.DatasetReaderProcessor.QUERY;
 import static com.viacom.arcops.nifi.NiFiProperties.FAILURE;
 import static com.viacom.arcops.nifi.NiFiProperties.SUCCESS;
 import static com.viacom.arcops.nifi.NiFiTestUtils.*;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
-public class DatasetReaderProcessorTest {
+class DatasetReaderProcessorTest {
     private TestRunner runner;
     private DBCPService dbcpService;
+    DatasetReaderProcessor processor;
+    private final Injector injector = prepareInjector();
 
     @BeforeEach
     void setup() {
-        final Injector injector = prepareInjector();
-        DatasetReaderProcessor processor = new DatasetReaderProcessor(injector);
+        processor = new DatasetReaderProcessor(injector);
         HashMap<String, String> processorParameters = new HashMap<String, String>() {{
             put(QUERY.getName(), "select param1, param2, param3 from any_table");
             put(BODY_COLUMN.getName(), "param3");
         }};
         runner = prepareTestRunnerFor(processor, processorParameters);
         dbcpService = injector.getInstance(DBCPService.class);
-        dbcpService = addH2DbcpService(dbcpService,runner,null);
+        dbcpService = addH2DbcpService(dbcpService, runner, null);
     }
 
     @Test
-    public void shouldRetrieveMessages() throws SQLException {
+    void shouldRetrieveMessages() throws SQLException {
         final int rowCount = 4;
         fillUpTable(rowCount);
 
@@ -55,8 +54,93 @@ public class DatasetReaderProcessorTest {
 
         List<MockFlowFile> success = runner.getFlowFilesForRelationship(SUCCESS);
         List<String> paramsFromFlowFiles = success.stream().map(mockFlowFile -> mockFlowFile.getAttribute("param1")).collect(Collectors.toList());
-        assertThat(paramsFromFlowFiles).containsAll(Arrays.asList("1","2","3","4"));
+        assertThat(paramsFromFlowFiles).containsAll(Arrays.asList("1", "2", "3", "4"));
     }
+
+    @Test
+    void shouldProcessQuery() {
+        String query = "#[propertyName]";
+        HashMap<String, String> propertiesMap = new HashMap<String, String>() {{
+            put("#[propertyName]", "key.value");
+        }};
+
+        String expectedQuery = processor.processDatasetQuery(query, propertiesMap);
+        assertThat(expectedQuery).isEqualTo("key.value");
+    }
+
+    @Test
+    void shouldProcessQueryWithDuplication() {
+        String query = "#[propertyName] and #[propertyName]";
+        HashMap<String, String> propertiesMap = new HashMap<String, String>() {{
+            put("#[propertyName]", "key.value");
+        }};
+
+        String expectedQuery = processor.processDatasetQuery(query, propertiesMap);
+        assertThat(expectedQuery).isEqualTo("key.value and key.value");
+    }
+
+    @Test
+    void shouldProcessQueryWith2Params() {
+        String query = "#12 and #21";
+        HashMap<String, String> propertiesMap = new HashMap<String, String>() {
+            {
+                put("#12", "key.value");
+                put("#21", "key.value2");
+            }
+        };
+
+        String expectedQuery = processor.processDatasetQuery(query, propertiesMap);
+        assertThat(expectedQuery).isEqualTo("key.value and key.value2");
+    }
+
+    @Test
+    void shouldProcessComplicatedQuery() {
+        String query = "Select * from table where property = propertyName";
+        HashMap<String, String> propertiesMap = new HashMap<String, String>() {{
+            put("propertyName", "key.value");
+        }};
+
+        String expectedQuery = processor.processDatasetQuery(query, propertiesMap);
+        assertThat(expectedQuery).isEqualTo("Select * from table where property = key.value");
+    }
+
+    @Test
+    void shouldNotProcessQuery() {
+        String query = "queryWithoutPattern";
+        String expectedQuery = processor.processDatasetQuery(query, Collections.emptyMap());
+        runner.run();
+        assertThat(expectedQuery).isEqualTo("queryWithoutPattern");
+    }
+
+    @Test
+    void shouldExtractProperProperties(){
+        runner.setProperty("Key1","value1");
+        runner.setProperty("#[Key2]","value2");
+        runner.setProperty("#dd","value2");
+        runner.setProperty("#21","value2");
+        runner.setProperty("#22","value2");
+        Map<String, String> matchingProperties = processor.getPropertiesWhichMatchesWithPattern(runner.getProcessContext(), "#\\d+");
+        assertThat(matchingProperties).containsOnlyKeys("#21","#22");
+    }
+
+    @Test
+    void shouldNotExtractAnyProperties(){
+        runner.setProperty("Key1","value1");
+        runner.setProperty("#[Key2]","value2");
+        runner.setProperty("#dd","value2");
+        Map<String, String> matchingProperties = processor.getPropertiesWhichMatchesWithPattern(runner.getProcessContext(), "#\\d+");
+        assertThat(matchingProperties).isEmpty();
+    }
+
+
+    @Test
+    void shouldNotExtractAnyPropertiesSecondTest(){
+        Map<String, String> matchingProperties = processor.getPropertiesWhichMatchesWithPattern(runner.getProcessContext(), "#\\d+");
+        assertThat(matchingProperties).isEmpty();
+    }
+
+
+
 
     private void fillUpTable(int rowCount) throws SQLException {
         try (Connection connection = dbcpService.getConnection()) {
